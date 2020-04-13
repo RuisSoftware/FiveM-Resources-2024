71dadc396e584c9b47985aad3ca4f26fddf3ffb6
dutchplayers <info@dutch-players.nl>
2020-04-13 13:31:29 +0200

    Now also supports shops

--- UPLOAD TO YOUR DATABASE.sql
+++ UPLOAD TO YOUR DATABASE.sql
@@ -15,3 +15,55 @@ CREATE TABLE IF NOT EXISTS `trunk_inventory` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `plate` (`plate`)
 );
+
+ALTER TABLE `items`
+	ADD COLUMN `price` int(11) NOT NULL DEFAULT 0
+;
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`, `price`) VALUES 
+('WEAPON_FLASHLIGHT', 'Flashlight', 1, 0, 1, 0),
+('WEAPON_STUNGUN', 'Taser', 100, 1, 1, 0),
+('WEAPON_KNIFE', 'Knife', 100, 1, 1, 0),
+('WEAPON_BAT', 'Baseball Bat', 1, 0, 1, 0),
+('WEAPON_PISTOL', 'Pistol', 100, 1, 1, 0),
+('WEAPON_PUMPSHOTGUN', 'Pump Shotgun', 1, 0, 1, 0),
+('9mm_rounds', '9mm Rounds', 20, 0, 1, 0),
+('shotgun_shells', 'Shotgun Shells', 20, 0, 1, 0),
+('cigarette', 'Cigarette', 20, 0, 1, 0),
+('lighter', 'Lighter', 20, 0, 1, 0),
+('rollingpaper', 'Rolling Paper', 20, 0, 1, 0),
+('phone', 'Phone', 20, 0, 1, 0),
+('sandwich', 'Sandwich', 20, 0, 1, 0),
+('hamburger', 'Hamburger', 20, 0, 1, 0),
+('cupcake', 'Cupcake', 20, 0, 1, 0),
+('chips', 'Chips', 20, 0, 1, 0),
+('pistachio', 'Pistachio', 20, 0, 1, 0),
+('chocolate', 'Chocolate', 20, 0, 1, 0),
+('cashew', 'Cashew', 20, 0, 1, 0),
+('cocacola', 'Coca-Cola', 20, 0, 1, 0),
+('drpepper', 'Dr Pepper', 20, 0, 1, 0),
+('energy', 'Energy Drink', 20, 0, 1, 0),
+('lemonade', 'Lemonade', 20, 0, 1, 0),
+('icetea', 'Ice-Tea', 20, 0, 1, 0),
+ ('beer', 'Beer', 20, 0, 1, 0),
+ ('wine', 'Wine', 20, 0, 1, 0),
+ ('vodka', 'Vodka', 20, 0, 1, 0),
+ ('tequila', 'Tequila', 20, 0, 1, 0),
+ ('whisky', 'Whisky', 20, 0, 1, 0),
+ ('grand_cru', 'Champagne', 20, 0, 1, 0),
+('drill', 'Drill', 20, 0, 1, 0),
+('binocular', 'Binoculars', 20, 0, 1, 0),
+('fixkit', 'Fix Kit', 20, 0, 1, 0),
+('gps', 'GPS', 20, 0, 1, 0),
+('lockpick', 'Lockpick', 20, 0, 1, 0),
+('scubagear', 'Scuba Gear', 20, 0, 1, 0),
+('blowtorch', 'Blowtorch', 20, 0, 1, 0),
+('1gbag', 'Ziploc Bag[1g]', 20, 0, 1, 0),
+('5gbag', 'Ziploc Bag[5g]', 20, 0, 1, 0),
+('50gbag', 'Ziploc Bag[50g]', 20, 0, 1, 0),
+('100gbag', 'Ziploc Bag[100g]', 20, 0, 1, 0),
+('lowgradefert', 'Fertilizer [low]', 20, 0, 1, 0),
+('highgradefert', 'Fertilizer [high]', 20, 0, 1, 0),
+('plantpot', 'Plant Pot', 20, 0, 1, 0),
+('drugscales', 'Scales', 20, 0, 1, 0)
+;
--- esx_inventoryhud/config.lua
+++ esx_inventoryhud/config.lua
@@ -8,3 +8,148 @@ Config.OpenControl = 289 -- Key for opening inventory. Edit html/js/config.js to
 
 -- List of item names that will close ui when used
 Config.CloseUiItems = {"phone", "weed_seed", "tunerchip", "fixkit", "medikit"}
+
+Config.ShopBlipID = 52
+Config.LiquorBlipID = 93
+Config.YouToolBlipID = 402
+Config.PrisonShopBlipID = 52
+Config.WeedStoreBlipID = 140
+Config.WeaponShopBlipID = 110
+
+Config.ShopLength = 14
+Config.LiquorLength = 10
+Config.YouToolLength = 2
+Config.PrisonShopLength = 2
+
+Config.Color = 2
+Config.WeaponColor = 1
+
+Config.WeaponLiscence = {x = 12.47, y = -1105.5, z = 29.8}
+Config.LicensePrice = 5000
+
+Config.Shops = {
+    RegularShop = {
+        Locations = {
+			{x = 373.875,   y = 325.896,  z = 102.566},
+			{x = 2557.458,  y = 382.282,  z = 107.622},
+			{x = -3038.939, y = 585.954,  z = 6.908},
+			{x = -3241.927, y = 1001.462, z = 11.830},
+			{x = 547.431,   y = 2671.710, z = 41.156},
+			{x = 1961.464,  y = 3740.672, z = 31.343},
+			{x = 2678.916,  y = 3280.671, z = 54.241},
+            {x = 1729.216,  y = 6414.131, z = 34.037},
+            {x = -48.519,   y = -1757.514, z = 28.421},
+			{x = 1163.373,  y = -323.801,  z = 68.205},
+			{x = -707.501,  y = -914.260,  z = 18.215},
+			{x = -1820.523, y = 792.518,   z = 137.118},
+            {x = 1698.388,  y = 4924.404,  z = 41.063},
+            {x = 25.723,   y = -1346.966, z = 28.497}, 
+        },
+        Items = {
+            {name = 'bread'},
+            {name = 'water'},
+            {name = 'cigarette'},
+            {name = 'lighter'},
+            {name = 'rollingpaper'},
+            {name = 'phone'},
+            {name = 'sandwich'},
+            {name = 'hamburger'},
+            {name = 'cupcake'},
+            {name = 'chips'},
+            {name = 'pistachio'},
+            {name = 'chocolate'},
+            {name = 'cashew'},
+            {name = 'cocacola'},
+            {name = 'drpepper'},
+            {name = 'energy'},
+            {name = 'lemonade'},
+            {name = 'icetea'}
+        }
+    },
+
+    RobsLiquor = {
+		Locations = {
+			{x = 1135.808,  y = -982.281,  z = 45.415},
+			{x = -1222.915, y = -906.983,  z = 11.326},
+			{x = -1487.553, y = -379.107,  z = 39.163},
+			{x = -2968.243, y = 390.910,   z = 14.043},
+			{x = 1166.024,  y = 2708.930,  z = 37.157},
+			{x = 1392.562,  y = 3604.684,  z = 33.980},
+			{x = -1393.409, y = -606.624,  z = 29.319}
+        },
+        Items = {
+            {name = 'beer'},
+            {name = 'wine'},
+            {name = 'vodka'},
+            {name = 'tequila'},
+            {name = 'whisky'},
+            {name = 'grand_cru'}
+        }
+	},
+
+    YouTool = {
+        Locations = {
+            {x = 2748.0, y = 3473.0, z = 55.68},
+        },
+        Items = {
+            {name = 'drill'},
+            {name = 'binocular'},
+            {name = 'fixkit'},
+            {name = 'gps'},
+            {name = 'lockpick'},
+            {name = 'scubagear'},
+            {name = 'blowtorch'},
+            {name = '1gbag'},
+            {name = '5gbag'},
+            {name = '50gbag'},
+            {name = '100gbag'},
+            {name = 'lowgradefert'},
+            {name = 'highgradefert'},
+            {name = 'plantpot'},
+            {name = 'drugscales'}
+        }
+    },
+
+    PrisonShop = {
+        Locations = {
+            {x = 1728.41, y = 2584.31, z = 45.84},
+        },
+        Items = {
+            {name = 'bread'},
+            {name = 'water'},
+            {name = 'cigarette'},
+            {name = 'lighter'},
+            {name = 'sandwich'},
+            {name = 'chips'}
+        }
+    },
+
+    WeaponShop = {
+        Locations = {
+            { x = -662.180, y = -934.961, z = 20.829 },
+            { x = 810.25, y = -2157.60, z = 28.62 },
+            { x = 1693.44, y = 3760.16, z = 33.71 },
+            { x = -330.24, y = 6083.88, z = 30.45 },
+            { x = 252.63, y = -50.00, z = 68.94 },
+            { x = 22.09, y = -1107.28, z = 28.80 },
+            { x = 2567.69, y = 294.38, z = 107.73 },
+            { x = -1117.58, y = 2698.61, z = 17.55 },
+            { x = 842.44, y = -1033.42, z = 27.19 },
+        },
+        Weapons = {
+            {name = "WEAPON_FLASHLIGHT", ammo = 1},
+            {name = "WEAPON_STUNGUN", ammo = 1},
+            {name = "WEAPON_KNIFE", ammo = 1},
+            {name = "WEAPON_BAT", ammo = 1},
+            {name = "WEAPON_PISTOL", ammo = 45},
+            {name = "WEAPON_PUMPSHOTGUN", ammo = 25}
+        },
+        Ammo = {
+            {name = "9mm_rounds", weaponhash = "WEAPON_PISTOL", ammo = 24},
+            {name = "shotgun_shells", weaponhash = "WEAPON_PUMPSHOTGUN", ammo = 12}
+        },
+        Items = {
+
+        }
+    },
+}
