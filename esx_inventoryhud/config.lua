f53c278c530ef2efa86c1cbf5061ad1cbe105a1a
dutchplayers <info@dutch-players.nl>
2020-04-13 13:10:17 +0200

    Update config.lua

--- esx_inventoryhud/config.lua
+++ esx_inventoryhud/config.lua
@@ -1,9 +1,9 @@
 Config = {}
 Config.Locale = 'nl'
-Config.IncludeCash = false -- Include cash in inventory?
-Config.IncludeWeapons = true -- Include weapons in inventory?
-Config.IncludeAccounts = true -- Include accounts (bank, black money, ...)?
-Config.ExcludeAccountsList = {"bank", "money"} -- List of accounts names to exclude from inventory
+Config.IncludeCash = false -- DONT TOUCH!
+Config.IncludeWeapons = true -- TRUE or FALSE
+Config.IncludeAccounts = true -- TRUE or FALSE
+Config.ExcludeAccountsList = {"bank", "money"} --  DONT TOUCH!
 Config.OpenControl = 289 -- Key for opening inventory. Edit html/js/config.js to change key for closing it.
 
 -- List of item names that will close ui when used
