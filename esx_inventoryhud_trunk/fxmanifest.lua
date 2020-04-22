abf73f1690744cd91cf89602ead2781d95b4ea58
Kevin O'Byrne <62954271+TehZombiJesus@users.noreply.github.com>
2020-04-22 10:04:10 -0400

    Update fxmanifest.lua

--- esx_inventoryhud_trunk/fxmanifest.lua
+++ esx_inventoryhud_trunk/fxmanifest.lua
@@ -13,6 +13,7 @@ server_scripts {
   "@es_extended/locale.lua",
   "locales/nl.lua",
   "locales/en.lua",
+  "locales/fr.lua",
   "config.lua",
   "server/classes/c_trunk.lua",
   "server/trunk.lua",
@@ -23,7 +24,17 @@ client_scripts {
   "@es_extended/locale.lua",
   "locales/nl.lua",
   "locales/en.lua",
+  "locales/fr.lua",
   "config.lua",
   "client/esx_trunk-cl.lua"
 }
 
+dependencies {
+  'es_extended',
+  'cron',
+  'esx_addonaccount',
+  'esx_addoninventory',
+  'esx_datastore',
+  'esx_license',
+  'B1G_NOTIFY'
+}
