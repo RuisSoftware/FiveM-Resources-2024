296e50f24537003b859a99b0f789b35cb70b6847
Kevin O'Byrne <62954271+TehZombiJesus@users.noreply.github.com>
2020-04-22 09:58:38 -0400

    Create fr.js

---
+++ esx_inventoryhud/html/locales/fr.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "Jeter";
+invLocale.useItem = "Utiliser";
+invLocale.giveItem = "Donner";
+invLocale.secondInventoryNotAvailable = "L'inventaire secondaire n'est pas disponible.";
