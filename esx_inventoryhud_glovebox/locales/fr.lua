a3b43ef60730ba4119ab946da4006d31111a81a6
Kevin O'Byrne <62954271+TehZombiJesus@users.noreply.github.com>
2020-04-22 10:02:09 -0400

    Add files via upload

---
+++ esx_inventoryhud_glovebox/locales/fr.lua
@@ -0,0 +1,13 @@
+Locales["fr"] = {
+  ["glovebox_closed"] = "La boîte à gants est fermée.",
+  ["no_veh_nearby"] = "Il n'y a pas de véhicule à proximité.",
+  ["glovebox_in_use"] = "Quelqu'un d'autre utilise cette boîte à gants.",
+  ["glovebox_full"] = "Cette boîte à gants est pleine.",
+  ["invalid_quantity"] = "Quantité invalide",
+  ["cant_carry_more"] = "Vous ne pouvez plus porter.",
+  ["nacho_veh"] = "Ce n'est pas votre véhicule.",
+  ["invalid_amount"] = "Montant invalide",
+  ["insufficient_space"] = "Espace insuffisant",
+  ["glovebox_info"] = "<h3>Boite à Gants</h3><br><strong>Plaque:</strong> %s<br><strong>Capacité:</strong> %s / %s",
+  ["player_inv_no_space"] = "Vous n'avez pas assez d'espace dans votre inventaire pour cet article!"
+}
