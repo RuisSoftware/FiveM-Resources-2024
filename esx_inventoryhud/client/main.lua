25aa3a0f05662b64b0e6b52af985151c5381ab5c
/the Headquarters <53178920+dutchplayers@users.noreply.github.com>
2020-06-23 18:27:52 +0200

    Update main.lua

--- esx_inventoryhud/client/main.lua
+++ esx_inventoryhud/client/main.lua
@@ -21,7 +21,7 @@ Citizen.CreateThread(
     function()
         while true do
             Citizen.Wait(0)
-            if IsControlJustReleased(0, Config.OpenControl) then
+            if IsControlJustReleased(0, Config.CloseControl) then
 			if isInInventory then
                 closeInventory()
 				end
