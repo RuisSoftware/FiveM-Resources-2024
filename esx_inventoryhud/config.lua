12165a44707ba7667bc5d2a4a6ee847bc6aa9152
/the Headquarters <53178920+dutchplayers@users.noreply.github.com>
2020-06-23 18:27:17 +0200

    Update config.lua

--- esx_inventoryhud/config.lua
+++ esx_inventoryhud/config.lua
@@ -5,6 +5,7 @@ Config.IncludeWeapons = true -- TRUE or FALSE
 Config.IncludeAccounts = true -- TRUE or FALSE
 Config.ExcludeAccountsList = {"bank", "money"} --  DONT TOUCH!
 Config.OpenControl = 289 -- Key for opening inventory. Edit html/js/config.js to change key for closing it.
+Config.CloseControl = 200
 
 -- List of item names that will close ui when used
 Config.CloseUiItems = {"phone", "weed_seed", "tunerchip", "fixkit", "medikit"}
