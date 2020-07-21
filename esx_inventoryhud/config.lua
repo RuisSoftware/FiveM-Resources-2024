675911009a5cf6e602e80214341a10dfb0159c03
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-07-21 20:42:20 +0100

    Config for job searching added

--- esx_inventoryhud/config.lua
+++ esx_inventoryhud/config.lua
@@ -6,6 +6,7 @@ Config.IncludeAccounts = true -- TRUE or FALSE
 Config.ExcludeAccountsList = {"bank", "money"} --  DONT TOUCH!
 Config.OpenControl = 289 -- Key for opening inventory. Edit html/js/config.js to change key for closing it.
 Config.CloseControl = 200
+Config.JobOnlyInventory = true -- True means that if your police or ambulance you can search players, false means anyone can search players
 
 -- List of item names that will close ui when used
 Config.CloseUiItems = {"phone", "weed_seed", "tunerchip", "fixkit", "medikit"} -- Add your own items here!
