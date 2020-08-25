1c5074e766fee1bce227794982d79d89fea7656d
NEIDER12 <36943294+NEIDER12@users.noreply.github.com>
2020-08-24 23:59:56 -0600

    Update config.lua
    Adding this to avoid dirty money bug.

--- esx_inventoryhud/config.lua
+++ esx_inventoryhud/config.lua
@@ -1,9 +1,9 @@
 Config = {}
 Config.Locale = 'nl'
-Config.IncludeCash = false -- DONT TOUCH!
+Config.IncludeCash = true -- DONT TOUCH!
 Config.IncludeWeapons = true -- TRUE or FALSE
 Config.IncludeAccounts = true -- TRUE or FALSE
-Config.ExcludeAccountsList = {"bank"} --  DONT TOUCH!
+Config.ExcludeAccountsList = {"bank", "money"} --  DONT TOUCH!
 Config.OpenControl = 289 -- Key for opening inventory. Edit html/js/config.js to change key for closing it.
 Config.CloseControl = 200
 Config.JobOnlyInventory = true -- True means that if your police or ambulance you can search players, false means anyone can search players
