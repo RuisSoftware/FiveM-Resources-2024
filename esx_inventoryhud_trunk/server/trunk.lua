7641bd63c0bc68fdbd8a2b868127111c3363f21c
Poke <47903164+PokeSer@users.noreply.github.com>
2020-10-24 18:27:19 +0200

    Really fix, sry for that

--- esx_inventoryhud_trunk/server/trunk.lua
+++ esx_inventoryhud_trunk/server/trunk.lua
@@ -77,10 +77,10 @@ function MakeDataStore(plate)
     local owned = getOwnedVehicle(plate)
     local dataStore = CreateDataStore(plate, owned, data)
     SharedDataStores[plate] = dataStore
-    MySQL.Sync.fetchAll('SELECT * FROM trunk_inventory WHERE plate = @plate', {
+    MySQL.Async.fetchAll('SELECT * FROM trunk_inventory WHERE plate = @plate', {
         ["@plate"] = plate
     }, function(result)
-        if result[1] ~= nil then
+        if result[1] == nil then
             MySQL.Async.execute('INSERT INTO trunk_inventory(plate,data,owned) VALUES (@plate, '{}', @owned)', {
                 ['@plate'] = plate,
                 ['@owned'] = owned,
