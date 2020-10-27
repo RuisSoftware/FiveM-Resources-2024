d399aabddcd06e49b4c909b43bce200ed7b58a29
Richardson Douglas <richardson.douglas.31@gmail.com>
2020-10-27 05:43:42 -0300

    Add Brazilian Portuguese

---
+++ html/locales/br.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "REMOVER";
+invLocale.useItem = "USAR";
+invLocale.giveItem = "DAR";
+invLocale.secondInventoryNotAvailable = "O SEGUNDO INVENTÁRIO NÃO ESTÁ DISPONÍVEL";
