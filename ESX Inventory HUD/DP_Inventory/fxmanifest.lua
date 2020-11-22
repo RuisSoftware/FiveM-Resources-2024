874422bae7080fc2adc6395da771d94a03d7e157
/the Headquarters <53178920+dutchplayers@users.noreply.github.com>
2020-11-22 06:17:30 +0100

    1.1

--- ESX Inventory HUD/DP_Inventory/fxmanifest.lua
+++ ESX Inventory HUD/DP_Inventory/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'DP_Inventory'
 author 'Dutch Players'
 description 'Inventory for ESX 1.2 inspired by esx_invntoryhud from Trsak'
-version '1.0'
+version '1.1'
 url 'https://github.com/dutchplayers/FiveM-Resources'
 
 ui_page 'html/ui.html'
