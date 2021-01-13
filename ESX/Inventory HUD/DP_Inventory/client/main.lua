08268dd0cab8bed8ef9af9dd99217dfba6464df3
AgenChicken24 <64702468+AgenChicken24@users.noreply.github.com>
2021-01-13 20:13:29 +0200

    Update main.lua

--- ESX/Inventory HUD/DP_Inventory/client/main.lua
+++ ESX/Inventory HUD/DP_Inventory/client/main.lua
@@ -19,6 +19,7 @@ Citizen.CreateThread(function()
 	while ESX == nil do TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end) Citizen.Wait(0) end
 	while ESX.GetPlayerData().job == nil do Citizen.Wait(10) end
 	PlayerData = ESX.GetPlayerData()
+	loadPlayerInventory()
 	Citizen.Wait(3000)
 end)
 
