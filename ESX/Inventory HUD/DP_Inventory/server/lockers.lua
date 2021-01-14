0a617d0a84603912b58792eb519085963106b43d
/the Headquarters <53178920+dutchplayers@users.noreply.github.com>
2021-01-14 05:27:23 +0100

    Update lockers.lua

--- ESX/Inventory HUD/DP_Inventory/server/lockers.lua
+++ ESX/Inventory HUD/DP_Inventory/server/lockers.lua
@@ -57,24 +57,25 @@ oldESX = false -- dont touch
 function PayLockerRent(d, h, m)
 	MySQL.Async.fetchAll('SELECT * FROM inventory_lockers', {}, function(result)
 		for i=1, #result, 1 do
-			local xPlayer = ESX.GetPlayerFromIdentifier(result[i].identifier)
+			owner = result[i].owner
+			local xPlayer = DP.GetPlayerFromIdentifier(result[i].owner)
 			if xPlayer then
 				xPlayer.removeAccountMoney('bank', Config.DailyLockerRentPrice)
 				TriggerClientEvent('mythic_notify:client:SendAlert', xPlayer.source, { type = 'inform', text = 'Je betaalde €'..Config.DailyLockerRentPrice..' voor de verhuur van kluisjes.', length = 8000 })
 			else
 				if oldESX then
-					MySQL.Sync.execute('UPDATE users SET bank = bank - @bank WHERE owner = @identifier', { ['@bank'] = Config.DailyLockerRentPrice, ['@identifier'] = result[i].identifier })
+					MySQL.Sync.execute('UPDATE users SET bank = bank - @bank WHERE owner = @identifier', { ['@bank'] = Config.DailyLockerRentPrice, ['@identifier'] = owner })
 				else
-					print("[^1"..GetCurrentResourceName().."^7] An error occured while removing money from "..result[i].identifier.." ("..tostring(Config.DailyLockerRentPrice).."$). The player is offline. Forcing removing money")
+					print("[^1"..GetCurrentResourceName().."^7] An error occured while removing money from "..owner.." ("..tostring(Config.DailyLockerRentPrice).."$). The player is offline. Forcing removing money")
 					MySQL.Async.fetchScalar('SELECT accounts FROM users WHERE identifier = @identifier', {
-						['@identifier'] = result[i].identifier
+						['@identifier'] = owner
 					}, function(result)
 						if result then
 							local foundAccounts = json.decode(result)            
 							foundAccounts.bank = foundAccounts.bank - Config.DailyLockerRentPrice
 							MySQL.Async.execute('UPDATE users SET accounts = @accounts WHERE identifier = @identifier', {
 								['@accounts'] = json.encode(foundAccounts),
-								['@identifier'] = result[i].identifier
+								['@identifier'] = owner
 							}, function()
 							end)
 						end
