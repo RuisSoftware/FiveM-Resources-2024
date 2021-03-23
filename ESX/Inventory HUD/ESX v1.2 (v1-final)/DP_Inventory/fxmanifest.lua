1f2bff1abde914ddd9a5a6f02e28c4c99d5c53b5
Dutch Players <53178920+dutchplayers@users.noreply.github.com>
2021-03-23 04:17:12 +0100

    Update fxmanifest.lua

--- ESX/Inventory HUD/ESX v1.2 (v1-final)/DP_Inventory/fxmanifest.lua
+++ ESX/Inventory HUD/ESX v1.2 (v1-final)/DP_Inventory/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'DP_Inventory'
 author 'Dutch Players'
 description 'Inventory for DP 1.2 inspired by esx_inventoryhud from Trsak'
-version '1.2.8.1'
+version '1.2.9'
 url 'https://github.com/dutchplayers/FiveM-Resources'
 
 ui_page 'html/ui.html'
