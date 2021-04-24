fb4e9f275030ae66f4d265bebf2fbd1c3899f0b0
Dutch Players <53178920+dutchplayers@users.noreply.github.com>
2021-04-24 23:11:04 +0200

    Update config.lua

--- Standalone/DP_3D_Radio/config.lua
+++ Standalone/DP_3D_Radio/config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
 Config.UseVIP = false -- Dependencie PXRP_VIP( https://github.com/dutchplayers/pxrp_vip )
-Config.UseESX = true -- To disable audio system if player is not in car and does not have radio item.
+Config.UseESX = true -- To disable audio system if player is not in car and does not have radio item. If set to false, players are always able to use the commands.
