b688d8b9662715e3e3fe9e32462ca9ec7ccda5e6
Dutch Players <info@dutch-players.nl>
2021-04-26 04:54:16 +0200

    Added C# version

---
+++ Standalone/DP_Object_Freezer/source/Client/Config.cs
@@ -0,0 +1,10 @@
+﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+
+namespace DP_Object_Freezer.Client.Config
+{
+    public class Config
+    {
+    }
+}
---
+++ Standalone/DP_Object_Freezer/source/Server/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CitizenFX.Core;
+using System.Collections.Generic;
+
+namespace DP_Object_Freezer.Server
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+            Debug.WriteLine("DP_Object_Freezer ingeladen.");
+        }
+    }
+}
