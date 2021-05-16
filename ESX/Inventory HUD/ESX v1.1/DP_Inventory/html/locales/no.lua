8e6389854d04c1cdf1280415dc049e0b3c2eba1a
0pax <64065031+0pax@users.noreply.github.com>
2021-05-16 14:47:18 +0200

    Create no.lua

---
+++ ESX/Inventory HUD/ESX v1.1/DP_Inventory/html/locales/no.lua
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "Kast";
+invLocale.useItem = "Bruk";
+invLocale.giveItem = "Gi";
+invLocale.secondInventoryNotAvailable = "Sekundært inventar er ikke i bruk.";
